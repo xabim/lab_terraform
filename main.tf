1e1950881803977655f884bf2b48c3c6e82fcda4
Javier Marti <xabiorama@gmail.com>
2017-12-01 19:45:52 +0100

    Changed dns support

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ provider "aws" {
 
 resource "aws_vpc" "vpc" {
     cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
+    enable_dns_hostnames = false
+    enable_dns_support = false
     tags {
         Name = "Openwebinars"
     }
