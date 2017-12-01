8ba3b0589f2922e653c834d8bf8acd6fff6c25d4
Javier Marti <xabiorama@gmail.com>
2017-12-01 20:35:41 +0100

    Changed vpc to vpc file

--- main.tf
+++ main.tf
@@ -7,11 +7,3 @@ provider "aws" {
     profile = "openwebinars"
 }
 
-resource "aws_vpc" "vpc" {
-    cidr_block = "10.8.0.0/16"
-    enable_dns_hostnames = false
-    enable_dns_support = false
-    tags {
-        Name = "Openwebinars"
-    }
-}
