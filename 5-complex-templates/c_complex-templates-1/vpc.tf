01273d2ffab1026587917d2b71c004006318ceea
Javier Marti <xabiorama@gmail.com>
2017-12-04 12:10:22 +0100

    Added Internet gateway

--- 5-complex-templates/c_complex-templates-1/vpc.tf
+++ 5-complex-templates/c_complex-templates-1/vpc.tf
@@ -46,3 +46,13 @@ resource "aws_subnet" "pri2" {
        Name = "pri2"
    }
 }
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_route" "default_route" {
+    route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+}
