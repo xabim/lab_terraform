89228760c5dc5dddd915ef79be818eb56054ed86
Javier Marti <xabiorama@gmail.com>
2017-12-04 20:08:49 +0100

    Deleting variables

--- 6-advanced-concepts/b_creating-modules/variables.tf
+++ 6-advanced-concepts/b_creating-modules/variables.tf
@@ -1,28 +1,3 @@
-variable "cidr" {
-    type = "string"
-    default = "10.0.0.0/16"
-}
-
-variable "pub1_cidr" {
-    type = "string"
-    default = "10.0.0.0/24"
-}
-
-variable "pub2_cidr" {
-    type = "string"
-    default = "10.0.1.0/24"
-}
-
-variable "pri1_cidr" {
-    type = "string"
-    default = "10.0.10.0/24"
-}
-
-variable "pri2_cidr" {
-    type = "string"
-    default = "10.0.11.0/24"
-}
-
 variable "instance_type" {
     type = "string"
     default = "t2.micro"
