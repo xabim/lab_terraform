e973a9cfc98aa76e641ff295e447978cbd382ea7
Javier Marti <xabiorama@gmail.com>
2017-12-04 12:29:58 +0100

    Added security group of rds

--- 5-complex-templates/d_complex-templates-2/security_groups.tf
+++ 5-complex-templates/d_complex-templates-2/security_groups.tf
@@ -31,3 +31,20 @@ resource "aws_security_group" "web-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "rds-sg" {
+    name = "rds-sg"
+    vpc_id = "${aws_vpc.vpc.id}"
+    ingress {
+        from_port = 3306
+        protocol = "tcp"
+        to_port = 3306
+        security_groups = ["${aws_security_group.web-sg.id}"]
+    }
+    egress {
+        from_port = 0
+        protocol = "-1"
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
