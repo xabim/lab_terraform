e10839c6cf042862aaef11015376153f5c0017b8
Javier Marti <xabiorama@gmail.com>
2017-12-04 20:09:00 +0100

    Renaming variables

--- 6-advanced-concepts/b_creating-modules/modules/vpc/variables.tf
+++ 6-advanced-concepts/b_creating-modules/modules/vpc/variables.tf
@@ -1,4 +1,4 @@
-variable "cidr" {
+variable "vpc_cidr" {
     type = "string"
     default = "10.0.0.0/16"
 }
@@ -23,7 +23,7 @@ variable "pri2_cidr" {
     default = "10.0.11.0/24"
 }
 
-variable "enable_dns_hostname" {
+variable "enable_dns_hostnames" {
     type = "string"
     default = true
 }
