9fe75fb149998aea1ab90fe2bab7d60beac5de1a
Javier Marti <xabiorama@gmail.com>
2017-12-04 15:32:18 +0100

    Enabled DNS hostnames

--- 5-complex-templates/d_complex-templates-2/vpc.tf
+++ 5-complex-templates/d_complex-templates-2/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc" {
     cidr_block = "${var.cidr}"
-    enable_dns_hostnames = false
-    enable_dns_support = false
+    enable_dns_hostnames = true
+    enable_dns_support = true
     tags {
         Name = "Openwebinars"
     }
