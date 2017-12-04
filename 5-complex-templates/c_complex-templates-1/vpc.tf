b9584815bdc24748d77481769a940e2aff7afe5a
Javier Marti <xabiorama@gmail.com>
2017-12-04 12:00:21 +0100

    Fixed typo

--- 5-complex-templates/c_complex-templates-1/vpc.tf
+++ 5-complex-templates/c_complex-templates-1/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "pub1" {
    vpc_id = "${aws_vpc.vpc.id}"
    cidr_block = "${var.pub1_cidr}"
-   mac_public_ip_on_launch = true
+   map_public_ip_on_launch = true
    availability_zone = "${data.aws_availability_zones.az.names[0]}"
    tags {
        Name = "pub1"
@@ -20,7 +20,7 @@ resource "aws_subnet" "pub1" {
 resource "aws_subnet" "pub2" {
    vpc_id = "${aws_vpc.vpc.id}"
    cidr_block = "${var.pub2_cidr}"
-   mac_public_ip_on_launch = true
+   map_public_ip_on_launch = true
    availability_zone = "${data.aws_availability_zones.az.names[1]}"
    tags {
        Name = "pub2"
@@ -30,7 +30,7 @@ resource "aws_subnet" "pub2" {
 resource "aws_subnet" "pri1" {
    vpc_id = "${aws_vpc.vpc.id}"
    cidr_block = "${var.pri1_cidr}"
-   mac_public_ip_on_launch = true
+   map_public_ip_on_launch = true
    availability_zone = "${data.aws_availability_zones.az.names[0]}"
    tags {
        Name = "pri1"
@@ -40,7 +40,7 @@ resource "aws_subnet" "pri1" {
 resource "aws_subnet" "pri2" {
    vpc_id = "${aws_vpc.vpc.id}"
    cidr_block = "${var.pri2_cidr}"
-   mac_public_ip_on_launch = true
+   map_public_ip_on_launch = true
    availability_zone = "${data.aws_availability_zones.az.names[1]}"
    tags {
        Name = "pri2"
