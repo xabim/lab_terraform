5931a6b8cbcb441541e4e817bae48c0f4f3a06c8
Javier Marti <xabiorama@gmail.com>
2017-12-04 10:28:23 +0100

    Security groups for instances

--- 5-complex-templates/c_complex-templates-1/security_groups.tf
+++ 5-complex-templates/c_complex-templates-1/security_groups.tf
@@ -14,3 +14,20 @@ resource "aws_security_group" "elb-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "web-sg" {
+    name = "web-sg"
+    vpc_id = "${aws_vpc.vpc.id}"
+    ingress {
+        from_port = 80
+        protocol = "http"
+        to_port = 80
+        security_groups = ["${aws_security_group.elb-sg.id}"]
+    }
+    egress {
+        from_port = 0
+        protocol = "-1"
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
